11a4f8027337d3c47c2341164e5f99f6f168f474
MurodkhonJalolov <murodkhanjalolov2004@gmail.com>
2021-08-05 16:56:06 +0500

    finished navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.scss';
+import Navbar from './Components/Navbar/Navbar';
 import Blog from './Components/blogScreen/blog';
-// import Screen from './Components/Screens/Screen';
 import PopularNews from './Components/Popular-news/PopularNews';
 import WorldScreen from './Components/world_news/world_screen';
 
 function App() {
   return (
     <div className="App">
-      {/* <Screen/> */}
+      <Navbar />
       <Blog/>
       <WorldScreen/>
       <PopularNews />
